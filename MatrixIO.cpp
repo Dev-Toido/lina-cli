8bdca8e325a80e90d692113d702953c0eb210f0c
Vivek Garai <garaivivek@gmail.com>
2026-04-18 18:36:47 +0530

    Add Matrix class with IO and demo
    Add MatrixIO.cpp which implements a Matrix class with dynamic 2D allocation, overloaded operator>> and operator<< for element-wise input/output, and a destructor to free memory. Includes a simple main that prompts for and reads a 3x3 matrix then prints it. Also adds the compiled binary a.exe.

---
+++ MatrixIO.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class Matrix
+{
+private:
+    int nrow, ncol;
+    int **matx;
+
+public:
+    Matrix(int row, int col)
+    {
+        nrow = row, ncol = col;
+        matx = new int *[row];
+        for (int i = 0; i < row; i++)
+        {
+            matx[i] = new int[col];
+        }
+    }
+    friend std::istream &operator>>(std::istream &is, Matrix &m)
+    {
+        for (int i = 0; i < m.nrow; ++i)
+        {
+            for (int j = 0; j < m.ncol; ++j)
+            {
+                cout << "Element (" << i + 1 << "*" << j + 1 << "):";
+                is >> m.matx[i][j]; // Read into matrix element
+            }
+        }
+        return is; // Return stream for chaining (e.g., cin >> m1 >> m2;)
+    }
+    friend std::ostream &operator<<(std::ostream &os, const Matrix &m)
+    {
+        for (int i = 0; i < m.nrow; ++i)
+        {
+            for (int j = 0; j < m.ncol; ++j)
+            {
+                os << m.matx[i][j] << "\t"; // Tab separated output
+            }
+            os << "\n"; // Newline after each row
+        }
+        return os; // Return stream for chaining (e.g., cout << m1 << m2;)
+    }
+    ~Matrix()
+    {
+        for (int i = 0; i < nrow; ++i)
+        {
+            delete[] matx[i];
+        }
+        delete[] matx;
+    }
+};
+
+int main()
+{
+    Matrix m1(3, 3);
+    // Matrix m2(3,3);
+    cout << "Enter the elements of the array: " << endl;
+    cin >> m1;
+    cout << m1;
+
+    return 0;
+}
