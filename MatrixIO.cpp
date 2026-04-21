6889fb7839d70308a37af6ebf8332b5ac0c4c46b
Vivek Garai <garaivivek@gmail.com>
2026-04-21 16:32:46 +0530

    Add matrix arithmetic and improve IO
    Refactor Matrix IO and add arithmetic operations: implement interactive operator>> and formatted operator<<, add Matrix operator+ and operator- with dimension checks, and scalar multiplication (member and friend overloads). Update main() to read two matrices (accepts a,b), demonstrate addition, subtraction and scalar multiplication, and prompt for scalar input. Also includes minor CLI prompt/formatting improvements and basic error handling for incompatible dimensions.

--- MatrixIO.cpp
+++ MatrixIO.cpp
@@ -17,29 +17,113 @@ public:
             matx[i] = new int[col];
         }
     }
-    friend std::istream &operator>>(std::istream &is, Matrix &m)
+    friend istream &operator>>(istream &is, Matrix &m)
     {
-        for (int i = 0; i < m.nrow; ++i)
+        cout << "Enter elements row wise:\n";
+
+        for (int i = 0; i < m.nrow; i++)
         {
-            for (int j = 0; j < m.ncol; ++j)
+            cout << "Enter " << i + 1 << "st row : ";
+            for (int j = 0; j < m.ncol; j++)
             {
-                cout << "Element (" << i + 1 << " * " << j + 1 << ") : ";
-                is >> m.matx[i][j]; // Read into matrix element
+                is >> m.matx[i][j];
             }
         }
-        return is; // Return stream for chaining (e.g., cin >> m1 >> m2;)
+        return is;
     }
-    friend std::ostream &operator<<(std::ostream &os, const Matrix &m)
+    friend ostream &operator<<(ostream &os, const Matrix &m)
     {
-        for (int i = 0; i < m.nrow; ++i)
+        cout << "_______________________" << endl;
+        cout << "| Your Final Matrix   |" << endl;
+        cout << "***********************" << endl;
+        cout << "" << endl;
+
+        for (int i = 0; i < m.nrow; i++)
         {
-            for (int j = 0; j < m.ncol; ++j)
+            os << "  | ";
+            for (int j = 0; j < m.ncol; j++)
             {
-                os << m.matx[i][j] << "\t"; // Tab separated output
+                os << m.matx[i][j] << " ";
             }
-            os << "\n"; // Newline after each row
+            os << "|\n";
         }
-        return os; // Return stream for chaining (e.g., cout << m1 << m2;)
+
+        return os;
+    }
+    // Addition
+    Matrix operator+(const Matrix &m)
+    {
+        if (nrow != m.nrow || ncol != m.ncol)
+        {
+            cout << "Matrix addition not possible!\n";
+            exit(0);
+        }
+
+        Matrix result(nrow, ncol);
+
+        for (int i = 0; i < nrow; i++)
+        {
+            for (int j = 0; j < ncol; j++)
+            {
+                result.matx[i][j] = matx[i][j] + m.matx[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    // Subtraction
+    Matrix operator-(const Matrix &m)
+    {
+        if (nrow != m.nrow || ncol != m.ncol)
+        {
+            cout << "Matrix subtraction not possible!\n";
+            exit(0);
+        }
+
+        Matrix result(nrow, ncol);
+
+        for (int i = 0; i < nrow; i++)
+        {
+            for (int j = 0; j < ncol; j++)
+            {
+                result.matx[i][j] = matx[i][j] - m.matx[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    // Scalar Multiplication (m1 * s)
+    Matrix operator*(int scalar)
+    {
+        Matrix result(nrow, ncol);
+
+        for (int i = 0; i < nrow; i++)
+        {
+            for (int j = 0; j < ncol; j++)
+            {
+                result.matx[i][j] = matx[i][j] * scalar;
+            }
+        }
+
+        return result;
+    }
+
+    // Scalar Multiplication (s * m1)
+    friend Matrix operator*(int scalar, const Matrix &m)
+    {
+        Matrix result(m.nrow, m.ncol);
+
+        for (int i = 0; i < m.nrow; i++)
+        {
+            for (int j = 0; j < m.ncol; j++)
+            {
+                result.matx[i][j] = m.matx[i][j] * scalar;
+            }
+        }
+
+        return result;
     }
     ~Matrix()
     {
@@ -53,15 +137,43 @@ public:
 
 int main()
 {
-    int a;
-    int b;
-    cout << "Enter no of Row and Colume for your matrix in(a b)form : ";
-    cin >> a >> b;
+    int a, b;
+    char buf;
 
-    Matrix m1(a,b);
-    cout << "Enter the elements of the array: " << endl;
+    cout << "Enter no of Row and Column for your matrix in(a,b) form: ";
+    cin >> a >> buf >> b;
+
+    Matrix m1(a, b);
+    Matrix m2(a, b);
+
+    cout << "\nMatrix 1:\n";
     cin >> m1;
-    cout << m1;
-    
+
+    cout << "\nMatrix 2:\n";
+    cin >> m2;
+
+    // Addition
+    Matrix sum = m1 + m2;
+    cout << "\nAddition Result:\n";
+    cout << sum;
+
+    // Subtraction
+    Matrix diff = m1 - m2;
+    cout << "\nSubtraction Result:\n";
+    cout << diff;
+
+    // Scalar Multiplication
+    int s;
+    cout << "\nEnter scalar value: ";
+    cin >> s;
+
+    Matrix scaled1 = m1 * s;
+    cout << "\nResult (m1 * s):\n";
+    cout << scaled1;
+
+    Matrix scaled2 = s * m1;
+    cout << "\nResult (s * m1):\n";
+    cout << scaled2;
+
     return 0;
 }
